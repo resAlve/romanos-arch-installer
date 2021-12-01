6b57c4b0537597abc590fbe5e73714babd906381
resAlve <80428540+resAlve@users.noreply.github.com>
2021-12-01 12:05:33 -0600

    Update script2-palarch.sh

--- script2-palarch.sh
+++ script2-palarch.sh
@@ -8,12 +8,10 @@ echo "MONTAJE DE PARTICIONES"
 mount /dev/sda2 /mnt
 mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
-echo "DESCARGA DEL SISTEMA OPERATIVO..."
+echo "DESCARGA E INSTALACION DEL SISTEMA OPERATIVO..."
 pacstrap /mnt base base-devel git wget curl linux linux-firmware grub os-prober efibootmgr networkmanager dhcpcd netctl wpa_supplicant dialog firefox libreoffice-fresh libreoffice-fresh-es hunspell-es_any hunspell-es_mx gnome wayland
-echo "GENERAR FSTAB"
 genfstab /mnt
 genfstab /mnt >> /mnt/etc/fstab
-echo "ENTRAR EN CHROOT"
 cp /romanos-arch-installer/chrootinstaller.sh /mnt/
 chmod +x /mnt/chrootinstaller.sh
 arch-chroot /mnt /chrootinstaller.sh
